60ade8746da84998f2fdf89cdea1ba22496cf753
fabrizio <fabrizio@zirak.it>
2014-03-08 17:40:17 +0100

    bCombat v0.16 RC2

--- mod.cpp
+++ mod.cpp
@@ -1,5 +1,5 @@
-name = "bCombat AI mod v0.15";
+name = "bCombat AI mod v0.16 RC2";
 actionName = "bCombat AI mod official forum thread";
 action = "http://forums.bistudio.com/showthread.php?171436-OPEN-BETA-SP-bCombat-infantry-AI-Mod";
 picture = "bcombat.paa";
-description = "bCombat AI mod v0.15";
+description = "bCombat AI mod v0.16 RC2";
